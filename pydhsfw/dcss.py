7f533fb52e6a57526a120468aa2bc40360948e2b
Giles Mullen <gcmullen@sbcglobal.net>
2020-09-10 21:25:30 -0700

    enhance message and factory base classes

--- pydhsfw/dcss.py
+++ pydhsfw/dcss.py
@@ -1,8 +1,9 @@
 from pydhsfw.messages import MessageIn, MessageOut, MessageFactory, MessageProcessor
 from pydhsfw.connections import TcpipClientConnection, TcpipSocketReader
 
+
+#Dcss Message Base Classes
 class DcssMessageIn():
-    _split_msg = None
 
     def __init__(self, split):
         self._split_msg = split
@@ -13,6 +14,10 @@ class DcssMessageIn():
     @staticmethod
     def _split(buffer:bytes):
         return buffer.decode('ascii').rstrip('\n\r\x00').split(' ')
+
+    @staticmethod
+    def extract_command(buffer:bytes):
+        return DcssMessageIn._split(buffer)[0]
 
     def get_command(self):
         return self._split_msg[0]
@@ -25,58 +30,25 @@ class DcssStoCMessage(MessageIn, DcssMessageIn):
     def __init__(self, split):
         DcssMessageIn.__init__(self, split)
 
-    @staticmethod
-    def parse(buffer:bytes):
-        return DcssStoCMessage(DcssStoCMessage._split(buffer))
+    @classmethod
+    def parse(cls, buffer:bytes):
+        
+        msg = None
+        
+        split = DcssMessageIn._split(buffer)
+        if split[0] == cls.get_type_id():
+            msg = cls(split)
+        
+        return msg
 
-    @staticmethod
-    def get_command():
-        pass
-
-class DcssStoCSendClientType(DcssStoCMessage):
-    def __init__(self, split):
-        super().__init__(split)
-
-    @staticmethod
-    def get_command():
-        return 'stoc_send_client_type'
-
-    @staticmethod
-    def parse(buffer:bytes):
-        split_msg = DcssStoCMessage._split(buffer)
-        if split_msg[0] == DcssStoCSendClientType.get_command():
-            return DcssStoCSendClientType(split_msg)
-        else:
-            return None
-
-class DcssStoHRegisterOperation(DcssStoCMessage):
-    def __init__(self, split):
-        super().__init__(split)
-
-    @staticmethod
-    def get_command():
-        return 'stoh_register_operation'
-
-    @staticmethod
-    def parse(buffer:bytes):
-        split_msg = DcssStoCMessage._split(buffer)
-        if split_msg[0] == DcssStoHRegisterOperation.get_command():
-            return DcssStoHRegisterOperation(split_msg)
-        else:
-            return None
-
-    def get_operation_name(self):
-        return self.get_args()[0]
-    
 class DcssMessageOut(MessageOut):
 
     def __init__(self):
         super().__init__()
+        self._split_msg = None
 
     def __str__(self):
         return " ".join(self._split_msg)
-
-    _split_msg = None
 
     def write(self)->bytes:
         buffer = None
@@ -86,39 +58,61 @@ class DcssMessageOut(MessageOut):
 
         return buffer
 
-    @staticmethod
-    def get_command():
-        pass
-
 class DcssCtoSMessage(DcssMessageOut):
     def __init__(self):
         super().__init__()
 
 
-class DcssCtoSClientIsHardware(DcssCtoSMessage):
+
+#Dcss Incoming Messages
+class DcssStoCSendClientType(DcssStoCMessage):
+    _type_id = 'stoc_send_client_type'
+    def __init__(self, split):
+        super().__init__(split)
+
+class DcssStoHRegisterOperation(DcssStoCMessage):
+    _type_id = 'stoh_register_operation'
+    def __init__(self, split):
+        super().__init__(split)
+
+    def get_operation_name(self):
+        return self.get_args()[0]
     
+
+
+#Dcss Outgoing Messages
+class DcssCtoSClientIsHardware(DcssCtoSMessage): 
+    _type_id = 'htos_client_is_hardware'
     def __init__(self, dhs_name:str):
         super().__init__()
-        self._split_msg = [DcssCtoSClientIsHardware.get_command(), dhs_name]
+        self._split_msg = [self.get_type_id(), dhs_name]
 
-    @staticmethod
-    def get_command():
-        return 'htos_client_is_hardware'
 
+
+#Dcss xos v1 Reader
 class DcssXOS1SocketReader(TcpipSocketReader):
-    def read(self, sock):
+    def read_socket(self, sock):
         return self._read(sock, msglen=200)
 
+
+
+#Message Factory
 class DcssMessageFactory(MessageFactory):
     def __init__(self):
         super().__init__()
 
-    def createMessage(self, raw_message:bytes):
-        dcss_msg = DcssStoCSendClientType.parse(raw_message)
-        if not dcss_msg:
-            dcss_msg = DcssStoHRegisterOperation.parse(raw_message)
-        return dcss_msg
-        
+    def create_message(self, raw_msg:bytes):
+        cmd = DcssMessageIn.extract_command(raw_msg)
+        return self._create_message(cmd, raw_msg)
+
+    #TODO[Giles]: Investigate a way to register messages, possibly using decorators.
+    def _register_messages(self):
+        self._register_message(DcssStoCSendClientType)
+        self._register_message(DcssStoHRegisterOperation)
+
+
+
+#DcssClientConnection
 class DcssClientConnection(TcpipClientConnection):
     def __init__(self, msg_processor:MessageProcessor):
         super().__init__(msg_processor, DcssXOS1SocketReader(), DcssMessageFactory())
