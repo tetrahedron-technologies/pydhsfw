1aa40db98ad3bdc15ae39bb416a22a4e3a8637a9
Scott Classen <sclassen@lbl.gov>
2020-10-05 20:02:04 -0700

    standalone test_automl.py works

--- tests/test_automl.py
+++ tests/test_automl.py
@@ -11,25 +11,42 @@ from dotty_dict import dotty
 from pydhsfw.messages import MessageFactory, MessageQueue, register_message
 from pydhsfw.connection import ConnectionBase, register_connection
 from pydhsfw.processors import register_message_handler
-from pydhsfw.http import *
+from pydhsfw.http import PostJsonRequestMessage, JsonResponseMessage, HttpClientTransport, MessageResponseReader, MessageRequestWriter, ResponseMessage
 from pydhsfw.dhs import Dhs, DhsContext, DhsInit, DhsStart
 
 _logger = logging.getLogger(__name__)
 
-@register_message('automl_query_request')
-class AutoMLQueryRequest(PostRequestMessage):
-    def __init__(self, query:str):
-        super().__init__('/v1/models/default:predict', params={'q':query, 'format':'json'})
+@register_message('automl_predict_request')
+class AutoMLPredictRequest(PostJsonRequestMessage):
+    def __init__(self, key:str, image:bytes):
 
+        encoded_image = base64.b64encode(image).decode('utf-8')
 
-@register_message('automl_query_response', 'automl')
-class AutoMLQueryResponse(JsonResponseMessage):
+        instances = {
+            'instances': [
+                {'image_bytes': {'b64': str(encoded_image)},
+                'key': key}
+            ]
+        }
+
+        super().__init__('/v1/models/default:predict', json=instances)
+
+@register_message('automl_predict_response', 'automl')
+class AutoMLPredictResponse(JsonResponseMessage):
     def __init__(self, response):
         super().__init__(response)
 
     @property
     def top_result(self):
-        return dotty(self.json)['RelatedTopics.0.Text']
+        return dotty(self.json)['predictions.0.detection_scores.0']
+    
+    @property
+    def top_bb(self):
+        return dotty(self.json)['predictions.0.detection_boxes.0']
+
+    @property
+    def top_classification(self):
+        return dotty(self.json)['predictions.0.detection_classes_as_text.0']
 
 class AutoMLMessageFactory(MessageFactory):
     def __init__(self):
@@ -43,7 +60,7 @@ class AutoMLClientTransport(HttpClientTransport):
         super().__init__(url, MessageResponseReader(), MessageRequestWriter(), config)
 
 @register_connection('automl')
-class DuckClientConnection(ConnectionBase):
+class AutoMLClientConnection(ConnectionBase):
     def __init__(self, url:str, incoming_message_queue:MessageQueue, outgoing_message_queue:MessageQueue, config:dict={}):
         super().__init__(url, AutoMLClientTransport(url, config), incoming_message_queue, outgoing_message_queue, AutoMLMessageFactory(), config)
 
@@ -54,27 +71,25 @@ def dhs_init(message:DhsInit, context:DhsContext):
 
 @register_message_handler('dhs_start')
 def dhs_start(message:DhsStart, context:DhsContext):
+
     port_number = 5000
     image_key = '1a2s3d4f5g'
-    with io.open('loop_nylon.jpg', 'rb') as image_file:
-       encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
-    instances = {
-        'instances': [
-            {'image_bytes': {'b64': str(encoded_image)},
-             'key': image_key}
-        ]
-    }
-    url = 'http://localhost:{}/v1/models/default:predict'.format(port_number)
+    filename = 'loop_nylon.jpg'
+    url = 'http://localhost:{}'.format(port_number)
 
-    #response = requests.post(url, data=json.dumps(instances))
-    #print(response.json())
-
-    context.create_connection('automl_conn', 'automl', url)
+    context.create_connection('automl_conn', 'automl', url, {'heartbeat_path': '/v1/models/default'})
     context.get_connection('automl_conn').connect()
-
     time.sleep(3)
+    with io.open(filename, 'rb') as image_file:
+        binary_image = image_file.read()
+    context.get_connection('automl_conn').send(AutoMLPredictRequest(image_key,binary_image))
 
-    context.get_connection('automl_conn').send(AutoMLQueryRequest('what is the meaning of life'))
+@register_message_handler('automl_predict_response')
+def automl_query_response(message:AutoMLPredictResponse, context:DhsContext):
+    _logger.info(message.top_result)
+    # do stuff n math n things
+    # figurte out which op
+    # send operation updates to dcss
 
 dhs = Dhs()
 dhs.start()
