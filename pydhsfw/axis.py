5a5e14969b73869a17574841215ef50154345556
Ravi Paragiri <ravi_hills@rediffmail.com>
2020-10-09 17:34:19 -0700

    Added  GetRequestMessage() and  FileResponseMessage() classes in axis.py

--- pydhsfw/axis.py
+++ pydhsfw/axis.py
@@ -3,10 +3,34 @@ import logging
 from typing import Any
 from pydhsfw.messages import IncomingMessageQueue, OutgoingMessageQueue, MessageFactory
 from pydhsfw.connection import ConnectionBase, register_connection
-from pydhsfw.http import HttpClientTransport, MessageResponseReader, MessageRequestWriter, ResponseMessage  
+from pydhsfw.http import HttpClientTransport, MessageResponseReader, MessageRequestWriter, ResponseMessage, GetRequestMessage, FileResponseMessage
 
 _logger = logging.getLogger(__name__)
 
+
+@register_message('axis_image_request')
+class GetRequestMessage(MessageOut):
+    def __init__(self, path:str, params:dict=None):
+        super().__init__()
+        self._path = path
+        self._params = params
+    
+    def write(self)->Request:
+        request = Request(RequestVerb.GET.value, self._path, params=self._params)
+        request.headers[Headers.DHS_REQUEST_TYPE_ID.value] = self.get_type_id()
+        return request
+
+    def __str__(self):
+        return f'{super().__str__()} {self._path} {self._params}'
+
+@register_message('axis_image_response')
+class FileResponseMessage(ResponseMessage):
+    def __init__(self, response):
+        super().__init__(response)
+
+    @property
+    def file(self):
+        return self._response.content
 
 class AxisMessageFactory(MessageFactory):
     def __init__(self):
