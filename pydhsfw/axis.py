e06a99508c283d4cc65c3b6e3db563f800007498
Ravi Paragiri <ravi_hills@rediffmail.com>
2020-10-09 18:51:05 -0700

    Made changes to AxisGetRequestMessage() and AxisImageResponseMessage() classes

--- pydhsfw/axis.py
+++ pydhsfw/axis.py
@@ -7,30 +7,15 @@ from pydhsfw.http import HttpClientTransport, MessageResponseReader, MessageRequ
 
 _logger = logging.getLogger(__name__)
 
-
 @register_message('axis_image_request')
-class GetRequestMessage(MessageOut):
-    def __init__(self, path:str, params:dict=None):
-        super().__init__()
-        self._path = path
-        self._params = params
-    
-    def write(self)->Request:
-        request = Request(RequestVerb.GET.value, self._path, params=self._params)
-        request.headers[Headers.DHS_REQUEST_TYPE_ID.value] = self.get_type_id()
-        return request
+class AxisImageRequestMessage(GetRequestMessage):
+    def __init__(self):
+        super().__init__('/axis-cgi/jpg/image.cgi')
 
-    def __str__(self):
-        return f'{super().__str__()} {self._path} {self._params}'
-
-@register_message('axis_image_response')
-class FileResponseMessage(ResponseMessage):
+@register_message('axis_image_response', 'axis')
+class AxisImageResponseMessage(FileResponseMessage):
     def __init__(self, response):
         super().__init__(response)
-
-    @property
-    def file(self):
-        return self._response.content
 
 class AxisMessageFactory(MessageFactory):
     def __init__(self):
