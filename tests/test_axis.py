c6192359efac9ff907288f91f7a2f11158a9dffd
Giles Mullen <gcmullen@sbcglobal.net>
2020-10-13 14:44:25 -0700

    rm unused imports, send img request, create rsp msg

--- tests/test_axis.py
+++ tests/test_axis.py
@@ -1,14 +1,11 @@
-import io
 from context import pydhsfw
 import logging
 import sys
 import time
 import signal
 from typing import Any
-from pydhsfw.messages import IncomingMessageQueue, OutgoingMessageQueue, MessageFactory, register_message
-from pydhsfw.connection import ConnectionBase, register_connection
 from pydhsfw.processors import register_message_handler
-from pydhsfw.http import HttpClientTransport, MessageResponseReader, MessageRequestWriter, GetRequestMessage, FileResponseMessage
+from pydhsfw.axis import AxisImageRequestMessage, AxisImageResponseMessage
 from pydhsfw.dhs import Dhs, DhsContext, DhsInit, DhsStart
 
 _logger = logging.getLogger(__name__)
@@ -21,12 +18,16 @@ def dhs_init(message:DhsInit, context:DhsContext):
 @register_message_handler('dhs_start')
 def dhs_start(message:DhsStart, context:DhsContext):
     url = 'http://141.211.27.126'
-    context.create_connection('axis_conn', 'axis', url, {'heartbeat_path': '/axis-cgi/jpg/image.cgi?resolution=640x360&text=0&clock=0&date=0'})
-    context.get_connection('axis_conn').connect()
+    conn = context.create_connection('axis_conn', 'axis', url, {'heartbeat_path': '/axis-cgi/jpg/image.cgi?resolution=640x360&text=0&clock=0&date=0'})
+    conn.connect()
     
     time.sleep(3)
 
-    _logger.info(message.top_result)
+    conn.send(AxisImageRequestMessage())
+
+@register_message_handler('axis_image_response')
+def axis_image_response(message:AxisImageResponseMessage, context:DhsContext):
+    _logger.info(f'Received image, {message.file_length} bytes')
 
 dhs = Dhs()
 dhs.start()
